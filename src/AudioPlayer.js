afcef0c3c32515d66cc10f2fcc75ee8cc5bfe289
taqin98 <taqinjunior56@gmail.com>
2024-04-20 11:58:54 +0700

    update sound

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -39,7 +39,7 @@ const AudioPlayer = ({cbPlayAudio}) => {
 
   return (
     <div>
-      <audio controls={false} ref={audioRef} src={uri + "assets/sounds/penjaga-hati.mp3"} type="audio/mp3" />
+      <audio controls={false} ref={audioRef} src={uri + "assets/sounds/lamunan.mp3"} type="audio/mp3" />
       <button className="controls-button"
       onClick={playAudio}>
       <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="music note list" xmlns="http://www.w3.org/2000/svg" fill="white" className="bi-music-note-list b-icon bi" data-v-5955985f=""><g data-v-5955985f=""><path d="M12 13c0 1.105-1.12 2-2.5 2S7 14.105 7 13s1.12-2 2.5-2 2.5.895 2.5 2z"></path><path fillRule="evenodd" d="M12 3v10h-1V3h1z"></path><path d="M11 2.82a1 1 0 0 1 .804-.98l3-.6A1 1 0 0 1 16 2.22V4l-5 1V2.82z"></path><path fillRule="evenodd" d="M0 11.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 7H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 .5 3H8a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"></path></g></svg>
